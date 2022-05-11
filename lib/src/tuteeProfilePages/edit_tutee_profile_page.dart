1fb488dc8adceee33d19905320a69b10c4804233
kuda214 <kudachivunga33@gmail.com>
2022-05-11 17:32:24 +0200

    💄 Update UI for Edit Tutee Profile

--- lib/src/tuteeProfilePages/edit_tutee_profile_page.dart
+++ lib/src/tuteeProfilePages/edit_tutee_profile_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:tutor_me/src/colorPalette.dart';
+import 'package:tutor_me/src/colorPalette.dart';
 
 class EditTuteeProfilePage extends StatefulWidget {
   @override
@@ -9,6 +9,272 @@ class EditTuteeProfilePage extends StatefulWidget {
 class _EditTuteeProfilePageState extends State<EditTuteeProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            right: 0,
+            left: 0,
+            child: Container(
+              height: 170,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/Pictures/flower.jpg"),
+                      fit: BoxFit.fill)),
+              child: Container(
+                padding: EdgeInsets.only(top: 60, left: 140),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        // text: "Rose Tumil",
+                        // style: DefaultTextStyle.of(context).style,
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: 'Rose Tamil',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25,
+                                shadows: <Shadow>[
+                                  Shadow(
+                                    offset: Offset(2.0, 2.0),
+                                    blurRadius: 6.0,
+                                    color: Color.fromARGB(255, 0, 0, 0),
+                                  ),
+                                ],
+                              )),
+                        ],
+                      ),
+                    ),
+                    TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Colors.deepOrangeAccent),
+                        foregroundColor:
+                            MaterialStateProperty.all<Color>(Colors.white),
+                      ),
+                      onPressed: null,
+                      child: Text("Tutee"),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 95,
+            child: Container(
+              height: 120,
+              width: MediaQuery.of(context).size.width - 220,
+              margin: EdgeInsets.symmetric(
+                horizontal: 12,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                image: DecorationImage(
+                    image: AssetImage("assets/Pictures/profilePic.jpg")),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    spreadRadius: 5,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 50,
+            child: Container(
+              height: 30,
+              width: MediaQuery.of(context).size.width - 270,
+              margin: EdgeInsets.symmetric(
+                horizontal: 0,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: Color.fromRGBO(214, 82, 7, 1),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    spreadRadius: 5,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 230,
+            left: 30,
+            child: Container(
+              height: 30,
+              width: 255,
+              child: Column(children: [
+                RichText(
+                    text: TextSpan(
+                        text: "Editing profile details for Rose Tumil...",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 13,
+                        )))
+              ]),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.black54,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 260,
+            left: 25,
+            child: Container(
+              height: 320,
+              width: 240,
+              margin: const EdgeInsets.all(15.0),
+              // padding: const EdgeInsets.all(3.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.deepOrangeAccent),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Padding(padding: Padding. ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter valid text';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                        hintText: "Enter new username",
+                        labelText: "Click here to Change Username",
+                        contentPadding:
+                            new EdgeInsets.fromLTRB(20.0, 10.0, 0, 10.0),
+                        labelStyle: TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
+                        )),
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter valid text';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                        hintText: "Location here",
+                        labelText: "Click here to Change Location",
+                        contentPadding:
+                            new EdgeInsets.fromLTRB(20.0, 10.0, 0, 10.0),
+                        labelStyle: TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
+                        )),
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter valid text';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                        hintText: "Bio here",
+                        labelText: "Click here to Change Bio",
+                        contentPadding:
+                            new EdgeInsets.fromLTRB(20.0, 10.0, 0, 10.0),
+                        labelStyle: TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
+                        )),
+                  ),
+                  Container(
+                    margin: new EdgeInsets.fromLTRB(20.0, 10.0, 0, 0),
+                    width: 200,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.black54),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromARGB(255, 54, 33, 27),
+                          blurRadius: 5,
+                          spreadRadius: 1,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                            "Select the following according to your Tutor preferences\n"),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(40, 5, 10, 0),
+                        ),
+                        // Padding(padding: Padding. ),
+                        DropdownButton<String>(
+                          hint: Text("Gender"),
+                          items: <String>['Female', 'Male', 'Other']
+                              .map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                          onChanged: (_) {},
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        ),
+                        DropdownButton<String>(
+                          hint: Text("Age"),
+                          items: <String>[
+                            '16-19',
+                            '20-23',
+                            '24-28',
+                            '29-35',
+                            '36-40'
+                          ].map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                          onChanged: (_) {},
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 600,
+            left: 100,
+            child: Container(
+              width: 120,
+              height: 30,
+              child: ElevatedButton(
+                onPressed: null,
+                child: Text("Save"),
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.deepOrangeAccent),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
