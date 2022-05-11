5eaadbc4ee1e7befb832a9545ea8087edc6c3ab4
kuda214 <kudachivunga33@gmail.com>
2022-05-11 17:28:43 +0200

    💄 Update Tutee edit profile

---
+++ lib/src/tuteeProfilePages/edit_tutee_profile_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+// import 'package:tutor_me/src/colorPalette.dart';
+
+class EditTuteeProfilePage extends StatefulWidget {
+  @override
+  _EditTuteeProfilePageState createState() => _EditTuteeProfilePageState();
+}
+
+class _EditTuteeProfilePageState extends State<EditTuteeProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
