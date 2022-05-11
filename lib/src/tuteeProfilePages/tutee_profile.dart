fc92b1753814351f02467ee10f04b92984aaeff2
SimphiweNdlovu <93398304+SimphiweNdlovu@users.noreply.github.com>
2022-05-11 18:47:40 +0200

    Update tutee_profile.dart

---
+++ lib/src/tuteeProfilePages/tutee_profile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+// import 'package:tutor_me/src/colorPalette.dart';
+import 'package:tutor_me/src/tuteeProfilePages/edit_tutee_profile_page.dart';
+
+class TuteeProfilePage extends StatefulWidget {
+  const TuteeProfilePage({Key? key}) : super(key: key);
+
+  @override
+  _TuteeProfilePageState createState() => _TuteeProfilePageState();
+}
+
+class _TuteeProfilePageState extends State<TuteeProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
