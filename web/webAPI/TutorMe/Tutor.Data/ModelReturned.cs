7ddad08bcf312482e0a9de141623f2a4e7553ca8
Maduna Thabo <u19116498@tuks.co.za>
2022-05-12 20:59:55 +0200

    🏗️ (API): Start Building the Tutor Web API, initial build

---
+++ web/webAPI/TutorMe/Tutor.Data/ModelReturned.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutors.Data {
+    public class ModelReturned {
+        public int Id { get; set; }
+        public int NumberOfRows { get; set; }
+        public bool Flag { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ web/webAPI/TutorMe/TutorMe/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TutorMe.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
