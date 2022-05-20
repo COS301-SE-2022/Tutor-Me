f6e4d90e1b0023be10530fdc7d062e6ba533a426
kuda214 <kudachivunga33@gmail.com>
2022-05-20 20:57:11 +0200

    💄 Added Landing Register or login page

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 import 'src/app.dart';
+import 'src/authorization/register_or_login.dart';
 
 void main() async {
   final settingsController = SettingsController(SettingsService());
 
   await settingsController.loadSettings();
-  runApp(const MaterialApp(home: MyApp()));
+  runApp(MaterialApp(home: RegisterOrLogin()));
 }
