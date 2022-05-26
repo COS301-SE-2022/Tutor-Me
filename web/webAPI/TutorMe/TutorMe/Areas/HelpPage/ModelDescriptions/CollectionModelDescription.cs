720e81e1ed4e97ae1e6617e9df8544fb2c85811c
Maduna Thabo <u19116498@tuks.co.za>
2022-05-26 11:33:17 +0200

    ⏪ (reset): remove old api

--- web/webAPI/TutorMe/TutorMe/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace TutorMe.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- web/webAPI/TutorMe/TutorMe/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TutorMe.WebApiApplication" Language="C#" %>
