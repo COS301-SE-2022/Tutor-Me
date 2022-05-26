00815f2605a321cc97266039eb26bff3f6384745
Maduna Thabo <u19116498@tuks.co.za>
2022-05-26 15:04:36 +0200

    🚧 (API): add Tutee Models

---
+++ web/Api/Api/Models/Tutee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Api.Models
+{
+    public partial class Tutee
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string IdNumber { get; set; }
+        public string Module { get; set; }
+        public string Email { get; set; }
+        public string Tutors { get; set; }
+    }
+}
