88b1cba1703d4ba1fc6f4a1b4df792314cb86b0b
Maduna Thabo <u19116498@tuks.co.za>
2022-05-26 14:58:41 +0200

    🚧 (API): define Tutor Model

---
+++ web/Api/Api/Models/Tutor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Api.Models
+{
+    public partial class Tutor
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string IdNumber { get; set; }
+        public string Modules { get; set; }
+        public string Email { get; set; }
+        public string Tutees { get; set; }
+    }
+}
