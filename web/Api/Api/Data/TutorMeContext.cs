27f8f34c153f66c39083a0cf1b7af5416292a647
Maduna Thabo <u19116498@tuks.co.za>
2022-05-26 14:55:58 +0200

    🚧 (API): create database context

---
+++ web/Api/Api/Data/TutorMeContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Api.Models;
+
+namespace Api.Data
+{
+    public partial class TutorMeContext : DbContext
+    {
+        public TutorMeContext()
+        {
+        }
+
+        public TutorMeContext(DbContextOptions<TutorMeContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Tutee> Tutees { get; set; }
+        public virtual DbSet<Tutor> Tutors { get; set; }
+
+        
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
