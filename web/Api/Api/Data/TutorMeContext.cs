3710824e3e209e3c566cc04710805878a05f005e
Maduna Thabo <u19116498@tuks.co.za>
2022-05-26 14:58:07 +0200

    🚧 (API): Define Database Context Model

--- web/Api/Api/Data/TutorMeContext.cs
+++ web/Api/Api/Data/TutorMeContext.cs
@@ -20,7 +20,88 @@ namespace Api.Data
         public virtual DbSet<Tutee> Tutees { get; set; }
         public virtual DbSet<Tutor> Tutors { get; set; }
 
-        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tutee>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("email");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("firstName");
+
+                entity.Property(e => e.IdNumber)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("idNumber");
+
+                entity.Property(e => e.Module)
+                    .IsUnicode(false)
+                    .HasColumnName("module");
+
+                entity.Property(e => e.Surname)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("surname");
+
+                entity.Property(e => e.Tutors)
+                    .IsUnicode(false)
+                    .HasColumnName("tutors");
+            });
+
+            modelBuilder.Entity<Tutor>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("email");
+
+                entity.Property(e => e.IdNumber)
+                    .IsRequired()
+                    .HasMaxLength(13)
+                    .IsUnicode(false)
+                    .HasColumnName("idNumber");
+
+                entity.Property(e => e.Modules)
+                    .IsUnicode(false)
+                    .HasColumnName("modules");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Surname)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("surname");
+
+                entity.Property(e => e.Tutees)
+                    .IsUnicode(false)
+                    .HasColumnName("tutees");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
 
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
