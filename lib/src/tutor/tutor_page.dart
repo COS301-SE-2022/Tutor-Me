a3ed1e26109e57afe4a25572c21e8c49e173a7f8
Simphiwe <u19027372@tuks.co.za>
2022-05-29 22:35:15 +0200

    🌱  (add file)  added tutor_page

---
+++ lib/src/tutor/tutor_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+// import 'package:tutor_me/modules/api.services.dart';
+// import 'package:tutor_me/modules/tutors.dart';
+import '../tutorProfilePages/tutor_profile_view.dart';
+import '../Navigation/nav_drawer.dart';
+import '../tuteeProfilePages/tutee_data.dart';
+import '../theme/themes.dart';
+
