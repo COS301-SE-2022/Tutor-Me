b6f6c28a03ac84acb66866d563ac4a8c376e1748
Simphiwe <u19027372@tuks.co.za>
2022-05-29 22:59:24 +0200

    ✅ (update) updated the tutor_page.dart

--- lib/src/tutor/tutor_page.dart
+++ lib/src/tutor/tutor_page.dart
@@ -69,7 +69,97 @@ class TutorState extends State<Tutor> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+      tutee.setAttributes(
+        "I am a hardworker,I absolutely love the field I am in.I'm constantly looking for ways to get things done",
+        'Evander, Secunda\n',
+        'Rose Tamil\n',
+        '21 years old\n',
+        'Female\n');
+    return DefaultTabController(
+      length: 3,
+      child: MaterialApp(
+        themeMode: ThemeMode.system,
+        darkTheme: Themes.darkTheme,
+  
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          drawer: const NavigationDrawerWidget(),
+          appBar: AppBar(
+            bottom: const TabBar(
+              indicatorColor: Colors.white,
+              tabs: [
+                Tab(icon: Icon(Icons.chat_bubble_rounded), text: 'Chat'),
+                Tab(icon: Icon(Icons.person), text: 'Request'),
+                Tab(icon: Icon(Icons.phone), text: 'Calls')
+              ],
+            ),
+            // backgroundColor: const Color(0xffD6521B),
+            centerTitle: true,
+            title: const Text('Tutor Me'),
+            actions: <Widget>[
+              IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+            ],
+          ),
+          body: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 400,
+                child: ListView.builder(
+                  padding: const EdgeInsets.all(10),
+                  itemCount: 5,
+                  itemBuilder: _cardBuilder,
+                ),
+              )
+
+              //ignore: unused_local_variable
+
+              // GestureDetector(
+              //   child: Card(
+              //     child: Column(
+              //       mainAxisSize: MainAxisSize.min,
+              //       children: <Widget>[
+              //         ListTile(
+              //           leading: const Icon(Icons.account_circle),
+              //           title: Text(person),
+              //           subtitle:
+              //               const Text('I am a Machenical engineer student'),
+              //         ),
+              //       ],
+              //     ),
+              //   ),
+              //   onTap: () {
+              //     Navigator.of(context).push(MaterialPageRoute(
+              //         builder: (BuildContext context) =>
+              //             TutorProfilePageView(person: person)));
+              //   },
+              // ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
-}
+
+  Widget _cardBuilder(BuildContext context, int i) {
+    return GestureDetector(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: const Icon(Icons.account_circle),
+              title: Text(tutors[i]),
+              subtitle: Text(bios[i]),
+            ),
+          ],
+        ),
+      ),
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (BuildContext context) =>
+                TutorProfilePageView(person: tutors[i], bio: bios[i], age: ages[i], location: location[i], gender: gender[i])));
+      },
+    );
+  }
+  }
+
