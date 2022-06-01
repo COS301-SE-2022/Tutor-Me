99a6211b4de78d6140573662e1398542c0b31f9b
kuda214 <kudachivunga33@gmail.com>
2022-06-02 01:05:02 +0200

    💄Added a color pallet

--- lib/src/colorpallete.dart
+++ lib/src/colorpallete.dart
@@ -5,3 +5,4 @@ const Color colorWhite = Colors.white;
 const Color colorBlue = Color(0xff5663ff);
 const Color colorGrey = Color(0xff979797);
 const Color colorOrange = Color.fromRGBO(214, 82, 27, 0.961);
+const Color colorTurqoise = Color.fromRGBO(17, 87, 72, 0.8);
