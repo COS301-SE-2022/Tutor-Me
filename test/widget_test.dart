2a6bed2ca30c90f4471b537c36838251ac6066ff
Maduna Thabo <u19116498@tuks.co.za>
2022-06-04 10:21:03 +0200

    🐛 (Test): unsused import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,7 +10,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:tutor_me/src/tutee_page.dart';
+// import 'package:tutor_me/src/tutee_page.dart';
 
 void main() {
   group('App dart', () {
