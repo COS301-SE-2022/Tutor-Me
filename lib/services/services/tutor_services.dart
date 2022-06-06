d36bb153744a878257b8b518cea6c7adbff5c237
Maduna Thabo <u19116498@tuks.co.za>
2022-06-06 14:03:36 +0200

    🚧 (Backend): seperate FE & BE

---
+++ lib/services/services/tutor_services.dart
@@ -0,0 +1,52 @@
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+import 'package:tutor_me/services/models/tutors.dart';
+
+class TutorServices {
+  static getTutors() async {
+    Uri tutorURL = Uri.https('tutormeapi.azurewebsites.net', '/api/Tutors');
+    try {
+      final response = await http.get(tutorURL, headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      });
+
+      if (response.statusCode == 200) {
+        String j = "";
+        if (response.body[0] != "[") {
+          j = "[" + response.body + "]";
+        } else {
+          j = response.body;
+        }
+        final List list = json.decode(j);
+        return list.map((json) => Tutors.fromObject(json)).toList();
+      } else {
+        throw Exception('Failed to load');
+      }
+    } catch (e) {
+      print(e);
+      throw Exception('Failed to load');
+    }
+   
+  }
+
+  static Future getTutor(String id) async {
+    Uri tutorURL = Uri.https('tutormeapi.azurewebsites.net', '/api/Tutors/$id');
+    try {
+      final response = await http.get(tutorURL, headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      });
+      if (response.statusCode == 200) {
+        return response;
+      } else {
+        throw Exception('Failed to load');
+      }
+    } catch (e) {
+      return null;
+    }
+  }
+}
