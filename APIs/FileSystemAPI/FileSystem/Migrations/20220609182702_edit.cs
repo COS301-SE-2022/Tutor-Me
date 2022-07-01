e1f3bee97524c4f556f2807ef2d297351a86a602
Thabo <madunathabo2@gmail.com>
2022-07-01 10:01:12 +0200

    🚧(API): initialize the file system api

---
+++ APIs/FileSystemAPI/FileSystem/Migrations/20220609182702_edit.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FileSystem.Migrations
+{
+    public partial class edit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "TutorImages",
+                newName: "Tuterid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Tuterid",
+                table: "TutorImages",
+                newName: "id");
+        }
+    }
+}
