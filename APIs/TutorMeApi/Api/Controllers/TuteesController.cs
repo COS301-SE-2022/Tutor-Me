0989e340ec9ee490bd9ed7fe62fb6e56943414c3
Simphiwe <u19027372@tuks.co.za>
2022-07-02 09:31:44 +0200

    Merge branch 'develop' of https://github.com/COS301-SE-2022/Tutor-ME into Simphiwe

---
+++ APIs/TutorMeApi/Api/Controllers/TuteesController.cs
@@ -0,0 +1,119 @@
+﻿using Api.Data;
+using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TuteesController : ControllerBase
+    {
+        private readonly TutorMeContext _context;
+
+        public TuteesController(TutorMeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tutees
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tutee>>> GetTutees()
+        {
+            if (_context.Tutees == null)
+            {
+                return NotFound();
+            }
+            return await _context.Tutees.ToListAsync();
+        }
+
+        // GET: api/Tutees/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tutee>> GetTutee(Guid id)
+        {
+            if (_context.Tutees == null)
+            {
+                return NotFound();
+            }
+            var tutee = await _context.Tutees.FindAsync(id);
+
+            if (tutee == null)
+            {
+                return NotFound();
+            }
+
+            return tutee;
+        }
+
+        // PUT: api/Tutees/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTutee(Guid id, Tutee tutee)
+        {
+            if (id != tutee.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tutee).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TuteeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tutees
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Tutee>> PostTutee(Tutee tutee)
+        {
+            if (_context.Tutees == null)
+            {
+                return Problem("Entity set 'TutorMeContext.Tutees'  is null.");
+            }
+            _context.Tutees.Add(tutee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTutee", new { id = tutee.Id }, tutee);
+        }
+
+        // DELETE: api/Tutees/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTutee(Guid id)
+        {
+            if (_context.Tutees == null)
+            {
+                return NotFound();
+            }
+            var tutee = await _context.Tutees.FindAsync(id);
+            if (tutee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tutees.Remove(tutee);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TuteeExists(Guid id)
+        {
+            return (_context.Tutees?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
