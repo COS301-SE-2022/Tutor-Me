b0bd4779da828fcb9c1010d00aa4e6d0f725ad89
Farai <u19068710@tuks.co.za>
2022-07-08 23:24:46 +0200

    🩹 (ignore uninitialized variable)

--- lib/src/authenticate/otp.dart
+++ lib/src/authenticate/otp.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
 import 'package:flutter/material.dart';
 import 'package:toggle_switch/toggle_switch.dart';
 import 'package:tutor_me/src/colorpallete.dart';
