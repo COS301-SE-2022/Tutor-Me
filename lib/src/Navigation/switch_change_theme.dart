2075a476540c57f0a6de2548491576059f81b96f
kuda214 <kudachivunga33@gmail.com>
2022-07-28 19:47:45 +0100

    Merged develop into  Kuda

---
+++ lib/src/Navigation/switch_change_theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:provider/provider.dart';
+import 'package:tutor_me/src/colorpallete.dart';
+import 'package:tutor_me/src/theme/themes.dart';
+
+import '../theme/themes.dart';
+// import 'package:theme_example/provider/theme_provider.dart';
+// import 'package:flutter/material.dart';/
+
+class ChangeThemeButtonWidget extends StatelessWidget {
+  const ChangeThemeButtonWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return  Row(
+      children: [
+        const Text("      Light Mode",style: TextStyle(fontSize: 15, color:  colorWhite ),),
+        Switch.adaptive(
+          value: themeProvider.isDarkMode,
+          onChanged: (value)  {
+            final provider = Provider.of<ThemeProvider>(context,listen: false);
+            provider.toggleTheme(value);
+
+          },
+        ),
+        const Text("   Dark Mode",style: TextStyle(fontSize: 15, color: colorWhite) ),
+
+
+      ],
+    );
+  }
+}
