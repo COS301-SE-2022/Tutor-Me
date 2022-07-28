73558fbe47e0a8bb9088af92397790bad194d158
Farai <u19068710@tuks.co.za>
2022-07-28 21:46:14 +0200

    🩹 💚 Fix CI Build

--- lib/src/Navigation/switch_change_theme.dart
+++ lib/src/Navigation/switch_change_theme.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 import 'package:provider/provider.dart';
 import 'package:tutor_me/src/colorpallete.dart';
