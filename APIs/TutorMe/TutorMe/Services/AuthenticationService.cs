d13fb34ef7a5cc5bfa524ac7216c8ec54633c750
Thabo <madunathabo2@gmail.com>
2022-08-20 00:24:56 +0200

    👔 (API & Database): init authentication, backend changes

---
+++ APIs/TutorMe/TutorMe/Services/AuthenticationService.cs
@@ -0,0 +1,41 @@
+﻿using TutorMe.Data;
+using TutorMe.Entities;
+using TutorMe.Models;
+
+namespace TutorMe.Services
+{
+    public interface IUserAuthenticationService
+    {
+        User LogInUser(UserLogIn user);
+    }
+    public class UserAuthenticationServices : IUserAuthenticationService
+    {
+
+        private TutorMeContext _context;
+        public UserAuthenticationServices(TutorMeContext context)
+        {
+            _context = context;
+        }
+        public User LogInUser(UserLogIn userDetails)
+        {
+            Console.Write(userDetails.Email);
+
+            var user = _context.User.FirstOrDefault(e => e.Email == userDetails.Email);
+            if (user != null)
+            {
+                if (user.Password == userDetails.Password)
+                {
+                    return user;
+                }
+                else
+                {
+                    throw new Exception("Wrong Username/Password");
+                }
+            }
+            else
+            {
+                throw new KeyNotFoundException("The user not found, Please register an account first.");
+            }
+        }
+    }
+}
