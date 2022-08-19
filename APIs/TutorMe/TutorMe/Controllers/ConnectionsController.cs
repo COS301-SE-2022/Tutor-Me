bfb1c6ccb080c73810abd88ad18c0eb89274dcb0
Thabo <madunathabo2@gmail.com>
2022-08-20 00:26:23 +0200

    👔 (API & Database): add connection service, backend changes

---
+++ APIs/TutorMe/TutorMe/Controllers/ConnectionsController.cs
@@ -0,0 +1,51 @@
+﻿using TutorMe.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TutorMe.Data;
+using TutorMe.Services;
+
+namespace TutorMe.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConnectionsController : ControllerBase
+    {
+        private IConnectionService connectionService;
+        private IMapper mapper;
+        private TutorMeContext _context;
+
+        public ConnectionsController(IConnectionService connectionService, IMapper mapper)
+        {
+            this.connectionService = connectionService;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllConnections()
+        {
+            var connections = connectionService.GetAllConnections();
+            return Ok(connections);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetConnectionById(Guid id)
+        {
+            var connection = connectionService.GetConnectionById(id);
+            return Ok(connection);
+        }
+
+        [HttpPost]
+        public IActionResult createConnection(Connection connection)
+        {
+            var connectionId = connectionService.createConnection(connection);
+            return Ok(connectionId);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteConnection(Guid id)
+        {
+            var connection = connectionService.deleteConnectionById(id);
+            return Ok(connection);
+        }
+    }
+}
---
+++ APIs/TutorMe/TutorMe/Services/ConnectionService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TutorMe.Data;
+using TutorMe.Models;
+
+namespace TutorMe.Services
+{
+    public interface IConnectionService
+    {
+        IEnumerable<Connection> GetAllConnections();
+        Connection GetConnectionById(Guid id);
+        Guid createConnection(Connection connection);
+        bool deleteConnectionById(Guid id);
+    }
+    public class ConnectionServices : IConnectionService
+    {
+
+        private TutorMeContext _context;
+        public ConnectionServices(TutorMeContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Connection> GetAllConnections()
+        {
+            return _context.Connection;
+        }
+
+        public Connection GetConnectionById(Guid id)
+        {
+            var connection = _context.Connection.Find(id);
+            if (connection == null)
+            {
+                throw new KeyNotFoundException("Connection not found");
+            }
+            return connection;
+        }
+        public Guid createConnection(Connection connection)
+        {
+            if (_context.Connection.Where(e => e.ModuleId == connection.ModuleId && e.TuteeId == connection.TuteeId && e.TutorId == connection.TutorId).Any())
+            {
+                throw new KeyNotFoundException("This Connection already exists, Please log in");
+            }
+            //Connection.Password = BCrypt.Net.BCrypt.HashPassword(Connection.Password, "ThisWillBeAGoodPlatformForBothConnectionsAndTuteesToConnectOnADailyBa5e5");
+            connection.ConnectionId = Guid.NewGuid();
+            _context.Connection.Add(connection);
+            _context.SaveChanges();
+            return connection.ConnectionId;
+        }
+
+        public bool deleteConnectionById(Guid id)
+        {
+            var connection = _context.Connection.Find(id);
+            if (connection == null)
+            {
+                throw new KeyNotFoundException("Connection not found");
+            }
+            _context.Connection.Remove(connection);
+            _context.SaveChanges();
+            return true;
+        }
+    }
+}
