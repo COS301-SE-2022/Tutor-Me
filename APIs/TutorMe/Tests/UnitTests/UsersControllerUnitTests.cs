81403aa57e080925e7ac8c1ec00095fe121f290f
Simphiwe <u19027372@tuks.co.za>
2022-08-23 14:51:32 +0200

    🚑 ✅ (unit test): refineUpdateUserNotFound

--- APIs/TutorMe/Tests/UnitTests/UsersControllerUnitTests.cs
+++ APIs/TutorMe/Tests/UnitTests/UsersControllerUnitTests.cs
@@ -81,32 +81,7 @@ public class UsersControllerUnitTests
             }
         };
         
-        DbContextOptionsBuilder<TutorMeContext> optionsBuilder = new();
-        var databaseName = MethodBase.GetCurrentMethod()?.Name;
-        if (databaseName != null)
-            optionsBuilder.UseInMemoryDatabase(databaseName);
-    
-      
-        using (TutorMeContext ctx = new(optionsBuilder.Options))
-        {
-            ctx.Add(newTutor);
-            ctx.SaveChangesAsync();
-        }
-    
-        //Modify the tutors Bio
-        newTutor.Bio = "Naruto fan";
-        var id = new Guid();
-        var unExsistingTutor = CreateTutor();
-        unExsistingTutor.Id = id;
-        Task<IActionResult> result;
-        using (TutorMeContext ctx1 = new(optionsBuilder.Options))
-        {
-            result =new TutorsController(ctx1).UpdateTutor(unExsistingTutor.Id,unExsistingTutor);
-        }
-    
-        // result should be of type NotFoundResult
-        Assert.IsType<NotFoundResult>(result.Result);
-
+        
         _userRepositoryMock.Setup(u => u.GetAllUsers()).Returns(users);
 
         var controller = new UsersController(_userRepositoryMock.Object, _mapper.Object);
@@ -228,5 +203,148 @@ public class UsersControllerUnitTests
         var actual = (result as OkObjectResult).Value;
         Assert.IsType<User>(actual);
     }
+    [Fact]
+    public async Task UpdateUser_WithUnExistingId_NotFound()
+    {
+        //Arrange
+        var user = new User
+        {
+            UserId =new Guid(),
+            FirstName ="Thabo",
+            LastName ="Maduna",
+            DateOfBirth ="02/04/2000",
+            Status = true,
+            Gender ="M",
+            Email ="thaboMaduna527@gmail.com",
+            Password ="24681012",
+            UserTypeId =new Guid(),
+            InstitutionId =new Guid(),
+            Location ="1166 TMN, 0028",
+            Bio = "The boys",
+            Year ="3",
+            Rating =0
+        };
+        //Act
+        var controller = new UsersController(_userRepositoryMock.Object,_mapper.Object);
+        var id = Guid.NewGuid();
+        var result =controller.UpdateUser(id,user);
+
+        //Assert     
+        Assert.IsType<BadRequestResult>(result);
+    }
+   // Test for UpdateUser returns Conflict if user is not registered
+    [Fact]
+    public async Task UpdateUser_With_An_UnExistingTutor_Conflict()
+    {
+        //Arrange
+        var user = new User
+        {
+            UserId =new Guid(),
+            FirstName ="Thabo",
+            LastName ="Maduna",
+            DateOfBirth ="02/04/2000",
+            Status = true,
+            Gender ="M",
+            Email ="thaboMaduna527@gmail.com",
+            Password ="24681012",
+            UserTypeId =new Guid(),
+            InstitutionId =new Guid(),
+            Location ="1166 TMN, 0028",
+            Bio = "The boys",
+            Year ="3",
+            Rating =0
+        };
+        
+        _userRepositoryMock.Setup(u => u.UpdateUser(user)).Throws<Exception>();
+        
+        //Act
+        var controller = new UsersController(_userRepositoryMock.Object,_mapper.Object);
+        var result =controller.UpdateUser(user.UserId,user);
+    
+        //Assert     
+        Assert.NotNull(result);
+        Assert.IsType<ConflictObjectResult>(result);
+
+    }
+    
+    //Test get all Tutors returns all the tutors
+    // [Fact]
+    // public async Task GetAllTutors_ReturnsAllTutors()
+    // {
+    //     //arrange
+    //     List<Tutor> users = new List<User>
+    //     {
+    //         new User
+    //         {
+    //             UserId =new Guid(),
+    //             FirstName ="Thabo",
+    //             LastName ="Maduna",
+    //             DateOfBirth ="02/04/2000",
+    //             Status = true,
+    //             Gender ="M",
+    //             Email ="thaboMaduna527@gmail.com",
+    //             Password ="24681012",
+    //             UserTypeId =new Guid(),
+    //             InstitutionId =new Guid(),
+    //             Location ="1166 TMN, 0028",
+    //             Bio = "The boys",
+    //             Year ="3",
+    //             Rating =0
+    //         },
+    //         new User
+    //         {
+    //             UserId =new Guid(),
+    //             FirstName ="Simphiwe",
+    //             LastName ="Ndlovu",
+    //             DateOfBirth ="02/04/1999",
+    //             Status = true,
+    //             Gender ="M",
+    //             Email ="simphiwendlovu527@gmail.com",
+    //             Password ="12345678",
+    //             UserTypeId =new Guid(),
+    //             InstitutionId =new Guid(),
+    //             Location ="1166 Burnett St, Hatfield, Pretoria, 0028",
+    //             Bio = "Naruto Fan",
+    //             Year ="3",
+    //             Rating =0
+    //         },
+    //         new User
+    //         {
+    //             UserId =new Guid(),
+    //             FirstName ="Kuda",
+    //             LastName ="Chivunga",
+    //             DateOfBirth ="28/03/2000",
+    //             Status = true,
+    //             Gender ="F",
+    //             Email ="kudaChivunga527@gmail.com",
+    //             Password ="147258369",
+    //             UserTypeId =new Guid(),
+    //             InstitutionId =new Guid(),
+    //             Location ="1166 TMN, 0028",
+    //             Bio = "The boys",
+    //             Year ="3",
+    //             Rating =0
+    //         }
+    //     };
+    //     
+    //     
+    //     _userRepositoryMock.Setup(u => u.GetAllUsers()).Returns(users);
+    //
+    //     var controller = new UsersController(_userRepositoryMock.Object, _mapper.Object);
+    //     var result = controller.GetAllUsers();
+    //
+    //
+    //     Assert.NotNull(result);
+    //     Assert.IsType<OkObjectResult>(result);
+    //         
+    //     var actual = (result as OkObjectResult).Value;
+    //     Assert.IsType<List<User>>(actual);
+    //     Assert.Equal(3, (actual as List<User>).Count);
+    //
+    //
+    // }
+    //
+
+
 
 }
