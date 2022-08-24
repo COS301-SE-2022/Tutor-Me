2e4157583f4f141db82496eddac4ded9745efa84
Simphiwe <u19027372@tuks.co.za>
2022-08-24 09:21:50 +0200

    ✅ 🚑 (unit test):GetRequestByTutorIdReturnsRequest

--- APIs/TutorMe/Tests/UnitTests/RequestControllerUnitTests.cs
+++ APIs/TutorMe/Tests/UnitTests/RequestControllerUnitTests.cs
@@ -93,6 +93,35 @@ public class RequestControllerUnitTests
         var actual = (result as OkObjectResult).Value;
         Assert.IsType<Request>(actual);
     }
+
+    // test for get request by tutor id
+    [Fact]
+    public async  Task GetRequestByTutorIdReturnsRequest()
+    {
+        //arrange
+        var Request = new Request
+        {
+            RequestId  = Guid.NewGuid(),
+            TuteeId  = Guid.NewGuid(),
+            TutorId  = Guid.NewGuid(),
+            DateCreated ="20/04/2020",
+            ModuleId=Guid.NewGuid()
+        };
+        
+        _RequestRepositoryMock.Setup(u => u.GetRequestByTutorById(Request.TutorId)).Returns(Request);
+        
+        var controller = new RequestsController(_RequestRepositoryMock.Object,_mapper.Object);
+        
+        //act
+        var result = controller.GetRequestByTutorById(Request.TutorId);
+        
+        Assert.NotNull(result);
+        Assert.IsType<OkObjectResult>(result);
+
+        var actual = (result as OkObjectResult).Value;
+        Assert.IsType<Request>(actual);
+    }
+ 
     
    
 }
