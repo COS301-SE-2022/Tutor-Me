60b3b9e0c2ded941d0830e31e3b709b19ef3d4ee
Thabo <madunathabo2@gmail.com>
2022-09-02 21:32:51 +0200

    (API): exception handling for requests endpoints

--- APIs/TutorMe/TutorMe/Services/ConnectionService.cs
+++ APIs/TutorMe/TutorMe/Services/ConnectionService.cs
@@ -10,6 +10,7 @@ namespace TutorMe.Services
         IEnumerable<Connection> GetAllConnections();
         IEnumerable<Connection> GetConnectionsByUserId(Guid id);
         Connection GetConnectionById(Guid id);
+        Guid createConnection(IConnection connection);
         bool deleteConnectionById(Guid id);
     }
     public class ConnectionServices : IConnectionService
@@ -35,6 +36,21 @@ namespace TutorMe.Services
         public IEnumerable<Connection> GetConnectionsByUserId(Guid id) {
             var connections = _context.Connection.Where(e => e.TuteeId == id || e.TutorId == id);
             return connections;
+        }
+        public Guid createConnection(IConnection connection)
+        {
+            if (_context.Connection.Where(e => e.ModuleId == connection.ModuleId && e.TuteeId == connection.TuteeId && e.TutorId == connection.TutorId).Any())
+            {
+                throw new KeyNotFoundException("This Connection already exists, Please log in");
+            }
+            var newConnection = new Connection();
+            newConnection.ConnectionId = Guid.NewGuid();
+            newConnection.ModuleId = connection.ModuleId;
+            newConnection.TuteeId = connection.TuteeId;
+            newConnection.TutorId = connection.TutorId;
+            _context.Connection.Add(newConnection);
+            _context.SaveChanges();
+            return connection.ConnectionId;
         }
 
         public bool deleteConnectionById(Guid id)
