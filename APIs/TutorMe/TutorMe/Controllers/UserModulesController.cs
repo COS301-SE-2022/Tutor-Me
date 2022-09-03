d026c2dc03d07f816c24fb484082215533927c01
Farai <u19068710@tuks.co.za>
2022-09-03 14:30:00 +0200

    💚 Fix CI/CD

---
+++ APIs/TutorMe/TutorMe/Controllers/UserModulesController.cs
@@ -0,0 +1,61 @@
+﻿using TutorMe.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TutorMe.Data;
+using TutorMe.Services;
+using TutorMe.Entities;
+
+namespace TutorMe.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserModulesController : ControllerBase {
+        private IUserModuleService userModuleService;
+        private IMapper mapper;
+        private TutorMeContext _context;
+
+        public UserModulesController(IUserModuleService userModuleService, IMapper mapper) {
+            this.userModuleService = userModuleService;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUserModules() {
+            Console.WriteLine("this is what we got");
+            var userModules = userModuleService.GetAllUserModules();
+            return Ok(userModules);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUserModulesByUserId(Guid id) {
+            var userModule = userModuleService.GetUserModulesByUserId(id);
+            if (userModule == null) {
+                return NotFound();
+            }
+            return Ok(userModule);
+        }
+
+        [HttpPost]
+        public IActionResult createUserModule(IUserModule userModule) {
+            try {
+                var userModuleId = userModuleService.createUserModule(userModule);
+                return Ok(userModuleId);
+            }
+            catch(Exception exception) {
+                return BadRequest(exception.Message);
+            }
+            
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUserModule(Guid id) {
+            try {
+                var userModule = userModuleService.deleteUserModuleById(id);
+                return Ok(userModule);
+            }
+            catch(Exception exception) {
+                return BadRequest(exception.Message);
+            }
+            
+        }
+    }
+}
---
+++ APIs/TutorMe/TutorMe/Services/UserModuleService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TutorMe.Data;
+using TutorMe.Entities;
+using TutorMe.Models;
+
+namespace TutorMe.Services {
+
+    public interface IUserModuleService {
+        IEnumerable<UserModule> GetAllUserModules();
+        //TODO: Fix this function
+        IEnumerable<Module> GetUserModulesByUserId(Guid id);
+        UserModule GetUserModuleById(Guid id);
+        Guid createUserModule(IUserModule userModule);
+        bool deleteUserModuleById(Guid id);
+    }
+    public class UserModuleServices: IUserModuleService {
+        private TutorMeContext _context;
+        public UserModuleServices(TutorMeContext context) {
+            _context = context;
+        }
+
+        public IEnumerable<UserModule> GetAllUserModules() {
+            return _context.UserModule;
+        }
+
+        public UserModule GetUserModuleById(Guid id) {
+            var userModule = _context.UserModule.Find(id);
+            if (userModule == null) {
+                throw new KeyNotFoundException("UserModule not found");
+            }
+            return userModule;
+        }
+        public Guid createUserModule(IUserModule userModuleInput) {
+            if (_context.UserModule.Where(e => e.UserId == userModuleInput.UserId && e.ModuleId == userModuleInput.ModuleId).Any()) {
+                throw new KeyNotFoundException("This UserModule already exists");
+            }
+            var userModule = new UserModule();
+            userModule.UserModuleId = Guid.NewGuid();
+            userModule.UserId = userModuleInput.UserId;
+            userModule.ModuleId = userModuleInput.ModuleId;
+            _context.UserModule.Add(userModule);
+            _context.SaveChanges();
+            return userModule.UserModuleId;
+        }
+
+        public IEnumerable<Module> GetUserModulesByUserId(Guid id) {
+            var userModules = _context.UserModule.Include(e=>e.Module).Where(e => e.UserId == id);
+            if(userModules == null) {
+                throw new KeyNotFoundException("User Module Record not found.");
+            }
+            var modules = new List<Module>();
+            foreach (UserModule item in userModules) {
+                modules.Add(item.Module);
+            }
+            return modules;
+        }
+
+        public bool deleteUserModuleById(Guid id) {
+            var userModule = _context.UserModule.Find(id);
+            if (userModule == null) {
+                throw new KeyNotFoundException("UserModule not found");
+            }
+            _context.Connection.Where(e => e.ModuleId == userModule.ModuleId).ToList().ForEach(e => _context.Connection.Remove(e));
+            _context.GroupMember.Where(e => e.Group.ModuleId == userModule.ModuleId).ToList().ForEach(e => _context.GroupMember.Remove(e));
+            _context.UserModule.Remove(userModule);
+            _context.SaveChanges();
+            return true;
+        }
+    }
+}
