d98eb4aa71dcfdc4e3aefcc8dbaf9a03af66cdd0
Thabo <madunathabo2@gmail.com>
2022-09-08 22:13:28 +0200

    (API): add authentication controller

---
+++ APIs/TutorMe/TutorMe/Controllers/AccountController.cs
@@ -0,0 +1,80 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TutorMe.Data;
+using TutorMe.Entities;
+using TutorMe.Models;
+using TutorMe.Services;
+
+namespace TutorMe.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase {
+        private readonly IJwtService _jwtService;
+        private readonly TutorMeContext _context;
+
+        public AccountController(IJwtService jwtService, TutorMeContext context) {
+            _jwtService = jwtService;
+            _context = context;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> AuthToken([FromBody] UserLogIn authRequest) {
+            if (!ModelState.IsValid)
+                return BadRequest(new AuthResponse { IsSuccess = false, Reason = "UserName and Password must be provided." });
+            var authResponse = await _jwtService.GetTokenAsync(authRequest, HttpContext.Connection.RemoteIpAddress.ToString());
+            if (authResponse == null)
+                return Unauthorized();
+            var user = _context.User.FirstOrDefault(e => e.Email == authRequest.Email);
+            return Ok(new {
+                token = authResponse.Token,
+                refreshToken = authResponse.RefreshToken,
+                user = user
+            });
+            //return Ok(authResponse);
+        }
+        [HttpPost("[action]")]
+        public async Task<IActionResult> RefreshToken([FromBody] RefreshTokenRequest request) {
+            if (!ModelState.IsValid)
+                return BadRequest(new AuthResponse { IsSuccess = false, Reason = "Tokens must be provided" });
+            string ipAddress = HttpContext.Connection.RemoteIpAddress.ToString();
+            var token = GetJwtToken(request.ExpiredToken);
+            var userRefreshToken = _context.UserRefreshToken.FirstOrDefault(
+                x => x.IsInvalidated == false && x.Token == request.ExpiredToken
+                && x.RefreshToken == request.RefreshToken
+                && x.IpAddress == ipAddress);
+
+            AuthResponse response = ValidateDetails(token, userRefreshToken);
+            if (!response.IsSuccess)
+                return BadRequest(response);
+
+            userRefreshToken.IsInvalidated = true;
+            _context.UserRefreshToken.Update(userRefreshToken);
+            await _context.SaveChangesAsync();
+
+            var userName = token.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.NameId).Value;
+            var authResponse = await _jwtService.GetRefreshTokenAsync(ipAddress, userRefreshToken.UserId,
+                userName);
+
+            return Ok(authResponse);
+        }
+
+        private AuthResponse ValidateDetails(JwtSecurityToken token, Models.UserRefreshToken userRefreshToken) {
+            if (userRefreshToken == null)
+                return new AuthResponse { IsSuccess = false, Reason = "Invalid Token Details." };
+            if (token.ValidTo > DateTime.UtcNow)
+                return new AuthResponse { IsSuccess = false, Reason = "Token not expired." };
+            if (!userRefreshToken.IsActive)
+                return new AuthResponse { IsSuccess = false, Reason = "Refresh Token Expired" };
+            return new AuthResponse { IsSuccess = true };
+        }
+
+        private JwtSecurityToken GetJwtToken(string expiredToken) {
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            return tokenHandler.ReadJwtToken(expiredToken);
+        }
+    }
+}
