b1f4da2c069375c91a202553bfb4be10a653aa57
Kuda Christine <62636817+Kuda214@users.noreply.github.com>
2022-09-12 17:04:01 +0100

    💄 (UI) Modified application

--- lib/src/Groups/tutee_group.dart
+++ lib/src/Groups/tutee_group.dart
@@ -66,7 +66,8 @@ class TuteeGroupPageState extends State<TuteeGroupPage> {
   getTutees() async {
     fetchToken().then((token) => setState(() => _token = token));
     try {
-      final tutees = await GroupServices.getGroupTutees(widget.group.getId, widget.globals);
+      final tutees = await GroupServices.getGroupTutees(
+          widget.group.getId, widget.globals);
       setState(() {
         tuteeList = tutees;
       });
@@ -80,8 +81,8 @@ class TuteeGroupPageState extends State<TuteeGroupPage> {
   getTuteeProfileImages() async {
     for (int i = 0; i < tuteeList.length; i++) {
       try {
-        final image =
-            await UserServices.getTuteeProfileImage(tuteeList[i].getId,widget.globals);
+        final image = await UserServices.getTuteeProfileImage(
+            tuteeList[i].getId, widget.globals);
         setState(() {
           tuteeImages.add(image);
         });
@@ -113,7 +114,8 @@ class TuteeGroupPageState extends State<TuteeGroupPage> {
 
   getTutor() async {
     try {
-      final tutor = await UserServices.getTutor(widget.group.getUserId, widget.globals);
+      final tutor =
+          await UserServices.getTutor(widget.group.getUserId, widget.globals);
 
       setState(() {
         tutorObj = tutor[0];
@@ -129,8 +131,8 @@ class TuteeGroupPageState extends State<TuteeGroupPage> {
 
   getTutorImage() async {
     try {
-      final image =
-          await UserServices.getTutorProfileImage(widget.group.getUserId, widget.globals);
+      final image = await UserServices.getTutorProfileImage(
+          widget.group.getUserId, widget.globals);
 
       setState(() {
         tutorHasImage = true;
@@ -161,7 +163,7 @@ class TuteeGroupPageState extends State<TuteeGroupPage> {
     if (provider.themeMode == ThemeMode.dark) {
       highlightColor = colorBlueTeal;
       textColor = colorWhite;
-      primaryColor = colorGrey;
+      primaryColor = colorOrange;
     } else {
       highlightColor = colorOrange;
       textColor = Colors.black;
@@ -193,68 +195,96 @@ class TuteeGroupPageState extends State<TuteeGroupPage> {
           : SingleChildScrollView(
               child: SizedBox(
                 height: screenHeight * 0.9,
-                width: screenWidth * 0.9,
+                width: screenWidth * 1,
                 child: Column(
                   children: <Widget>[
                     SizedBox(
                       height: screenHeight * 0.04,
                     ),
-                    Container(
-                      color: Colors.grey.withOpacity(0.2),
-                      height: screenHeight * 0.2,
-                      width: screenWidth * 0.42,
-                      padding: EdgeInsets.only(
-                          top: screenHeight * 0.02,
-                          bottom: screenHeight * 0.02,
-                          left: screenWidth * 0.02,
-                          right: screenWidth * 0.02),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text(
-                                'Group Header:',
-                                textAlign: TextAlign.left,
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w700,
-                                    fontSize: screenHeight * 0.03,
-                                    decoration: TextDecoration.underline),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(
+                              left: MediaQuery.of(context).size.width * 0.05),
+                          child: Container(
+                            width: screenWidth * 0.03,
+                            height: screenHeight * 0.2,
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage('assets/Pictures/group.jpg'),
+                                fit: BoxFit.cover,
                               ),
-                            ],
+                            ),
                           ),
-                          SizedBox(
-                            height: screenHeight * 0.01,
+                        ),
+                        Container(
+                          height: screenHeight * 0.2,
+                          width: screenWidth * 0.40,
+                          padding: EdgeInsets.only(
+                              top: screenHeight * 0.02,
+                              bottom: screenHeight * 0.02,
+                              left: screenWidth * 0.02,
+                              right: screenWidth * 0.02),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                                bottomRight: Radius.circular(
+                                    MediaQuery.of(context).size.width * 0.02),
+                                topRight: Radius.circular(
+                                    MediaQuery.of(context).size.width * 0.02)),
+                            color: Colors.grey.withOpacity(0.2),
                           ),
-                          Flexible(
-                            child: ConstrainedBox(
-                              constraints: const BoxConstraints.expand(),
-                              child: Theme(
-                                data: Theme.of(context).copyWith(
-                                    scrollbarTheme: ScrollbarThemeData(
-                                        thumbColor: MaterialStateProperty.all(
-                                            colorOrange))),
-                                child: Scrollbar(
-                                  child: Text(
-                                    widget.group.getDescription,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Text(
+                                    'Group Header:',
+                                    textAlign: TextAlign.left,
                                     style: TextStyle(
-                                      color: textColor,
-                                      fontSize: 16,
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: screenHeight * 0.03,
+                                        decoration: TextDecoration.underline),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: screenHeight * 0.01,
+                              ),
+                              Flexible(
+                                child: ConstrainedBox(
+                                  constraints: const BoxConstraints.expand(),
+                                  child: Theme(
+                                    data: Theme.of(context).copyWith(
+                                        scrollbarTheme: ScrollbarThemeData(
+                                            thumbColor:
+                                                MaterialStateProperty.all(
+                                                    colorOrange))),
+                                    child: Scrollbar(
+                                      child: Text(
+                                        widget.group.getDescription,
+                                        style: TextStyle(
+                                          color: textColor,
+                                          fontSize: 16,
+                                        ),
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                     SizedBox(
                       height: screenHeight * 0.03,
                     ),
                     SizedBox(
+                      width: screenWidth * 0.8,
                       height: screenHeight * 0.24,
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -270,7 +300,6 @@ class TuteeGroupPageState extends State<TuteeGroupPage> {
                             },
                             child: Card(
                               elevation: 0,
-                              color: Colors.transparent,
                               child: ListTile(
                                 horizontalTitleGap: screenHeight * 0.04,
                                 leading: Icon(
@@ -380,7 +409,7 @@ class TuteeGroupPageState extends State<TuteeGroupPage> {
                       ),
                     ),
                     SizedBox(
-                      height: screenHeight * 0.12,
+                      height: screenHeight * 0.14,
                       width: screenWidth * 0.5,
                       child: ListView.separated(
                           controller: ScrollController(),
