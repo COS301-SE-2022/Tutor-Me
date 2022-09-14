0bed00a1472c34cbf64075d77205963f8abed0d3
Thabo <madunathabo2@gmail.com>
2022-09-14 10:59:24 +0200

    (API): create event model

---
+++ APIs/TutorMe/TutorMe/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TutorMe.Models {
+    public class Event {
+        public Guid EventId { get; set; }
+        public Guid GroupId { get; set; }
+        public Guid UserId { get; set; }
+        public string DateOfEvent { get; set; }
+        public string VideoLink { get; set; }
+
+        [JsonIgnore]
+        public virtual Group Group { get; set; }
+        [JsonIgnore]
+        public virtual User User { get; set; }
+    }
+}
