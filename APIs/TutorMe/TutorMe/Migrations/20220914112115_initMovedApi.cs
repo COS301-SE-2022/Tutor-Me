3c04462d98b9d43d0ef9df743c1ee2ee72eb1a83
Thabo <madunathabo2@gmail.com>
2022-09-15 11:32:37 +0200

    (API): fix user creation endpoint

---
+++ APIs/TutorMe/TutorMe/Migrations/20220914112115_initMovedApi.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TutorMe.Migrations
+{
+    public partial class initMovedApi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Institution",
+                columns: table => new
+                {
+                    InstitutionId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: false, defaultValueSql: "(newid())"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Institution", x => x.InstitutionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserType",
+                columns: table => new
+                {
+                    UserTypeId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: false, defaultValueSql: "(newid())"),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserType", x => x.UserTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Module",
+                columns: table => new
+                {
+                    ModuleId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: false, defaultValueSql: "(newid())"),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ModuleName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    InstitutionId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: false, defaultValueSql: "(newid())"),
+                    Faculty = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Year = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Module", x => x.ModuleId);
+                    table.ForeignKey(
+                        name: "Modules_Institution_FK",
+                        column: x => x.InstitutionId,
+                        principalTable: "Institution",
+                        principalColumn: "InstitutionId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: false, defaultValueSql: "(newid())"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfBirth = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserTypeId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: false, defaultValueSql: "(newid())"),
+                    InstitutionId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: false, defaultValueSql: "(newid())"),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Bio = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Year = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    rating = table.Column<int>(type: "int", nullable: true, defaultValue: 0),
+                    NumberOfReviews = table.Column<int>(type: "int", nullable: true, defaultValue: 0)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserId);
+                    table.ForeignKey(
+                        name: "User_Institutioin_FK",
+                        column: x => x.InstitutionId,
+                        principalTable: "Institution",
+                        principalColumn: "InstitutionId");
+                    table.ForeignKey(
+                        name: "User_UserType_FK",
+                        column: x => x.UserTypeId,
+                        principalTable: "UserType",
+                        principalColumn: "UserTypeId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Connection",
+                columns: table => new
+                {
+                    ConnectionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TutorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TuteeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ModuleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Connection", x => x.ConnectionId);
+                    table.ForeignKey(
+                        name: "FK_Connection_Module_ModuleId",
+                        column: x => x.ModuleId,
+                        principalTable: "Module",
+                        principalColumn: "ModuleId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Connection_User_TuteeId",
+                        column: x => x.TuteeId,
+                        principalTable: "User",
+                        principalColumn: "UserId");
+                    table.ForeignKey(
+                        name: "FK_Connection_User_TutorId",
+                        column: x => x.TutorId,
+                        principalTable: "User",
+                        principalColumn: "UserId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Group",
+                columns: table => new
+                {
+                    GroupId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: false, defaultValueSql: "(newid())"),
+                    ModuleId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: false, defaultValueSql: "(newid())"),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    VideoId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Group", x => x.GroupId);
+                    table.ForeignKey(
+                        name: "Group_Module_FK",
+                        column: x => x.ModuleId,
+                        principalTable: "Module",
+                        principalColumn: "ModuleId");
+                    table.ForeignKey(
+                        name: "Group_User_FK",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Request",
+                columns: table => new
+                {
+                    RequestId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: false, defaultValueSql: "(newid())"),
+                    TuteeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TutorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateCreated = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ModuleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Request", x => x.RequestId);
+                    table.ForeignKey(
+                        name: "Requests_Module_FK",
+                        column: x => x.ModuleId,
+                        principalTable: "Module",
+                        principalColumn: "ModuleId");
+                    table.ForeignKey(
+                        name: "Requests_Tutee_FK",
+                        column: x => x.TuteeId,
+                        principalTable: "User",
+                        principalColumn: "UserId");
+                    table.ForeignKey(
+                        name: "Requests_Tutor_FK",
+                        column: x => x.TutorId,
+                        principalTable: "User",
+                        principalColumn: "UserId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserModule",
+                columns: table => new
+                {
+                    UserModuleId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: false, defaultValueSql: "(newid())"),
+                    ModuleId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: false, defaultValueSql: "(newid())"),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: false, defaultValueSql: "(newid())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserModule", x => x.UserModuleId);
+                    table.ForeignKey(
+                        name: "UserModule_Group_FK",
+                        column: x => x.ModuleId,
+                        principalTable: "Module",
+                        principalColumn: "ModuleId");
+                    table.ForeignKey(
+                        name: "UserModule_User_FK",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserRefreshToken",
+                columns: table => new
+                {
+                    UserRefreshTokenId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Token = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RefreshToken = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExpirationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IpAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsInvalidated = table.Column<bool>(type: "bit", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRefreshToken", x => x.UserRefreshTokenId);
+                    table.ForeignKey(
+                        name: "FK_UserRefreshToken_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Event",
+                columns: table => new
+                {
+                    EventId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: false, defaultValueSql: "(newid())"),
+                    GroupId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: false, defaultValueSql: "(newid())"),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: false, defaultValueSql: "(newid())"),
+                    DateOfEvent = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VideoLink = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Event", x => x.EventId);
+                    table.ForeignKey(
+                        name: "Events_Group_FK",
+                        column: x => x.GroupId,
+                        principalTable: "Group",
+                        principalColumn: "GroupId");
+                    table.ForeignKey(
+                        name: "Events_User_FK",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GroupMember",
+                columns: table => new
+                {
+                    GroupMemberId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: false, defaultValueSql: "(newid())"),
+                    GroupId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: false, defaultValueSql: "(newid())"),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: false, defaultValueSql: "(newid())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupMember", x => x.GroupMemberId);
+                    table.ForeignKey(
+                        name: "GroupMembers_Group_FK",
+                        column: x => x.GroupId,
+                        principalTable: "Group",
+                        principalColumn: "GroupId");
+                    table.ForeignKey(
+                        name: "GroupMembers_User_FK",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connections_ModuleId",
+                table: "Connection",
+                column: "ModuleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connections_TuteeId",
+                table: "Connection",
+                column: "TuteeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connections_TutorId",
+                table: "Connection",
+                column: "TutorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Event_GroupId",
+                table: "Event",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Event_GroupId1",
+                table: "Event",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Event_UserId",
+                table: "Event",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Group_ModuleId",
+                table: "Group",
+                column: "ModuleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Group_UserId",
+                table: "Group",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupMember_GroupId",
+                table: "GroupMember",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupMembers_UserId",
+                table: "GroupMember",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Modules_InstitutionId",
+                table: "Module",
+                column: "InstitutionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_ModuleId",
+                table: "Request",
+                column: "ModuleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_TuteeId",
+                table: "Request",
+                column: "TuteeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_TutorId",
+                table: "Request",
+                column: "TutorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_InstitutionId",
+                table: "User",
+                column: "InstitutionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_UserTypeId",
+                table: "User",
+                column: "UserTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserModule_ModuleId",
+                table: "UserModule",
+                column: "ModuleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserModule_UserId",
+                table: "UserModule",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRefreshToken_UserId",
+                table: "UserRefreshToken",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Connection");
+
+            migrationBuilder.DropTable(
+                name: "Event");
+
+            migrationBuilder.DropTable(
+                name: "GroupMember");
+
+            migrationBuilder.DropTable(
+                name: "Request");
+
+            migrationBuilder.DropTable(
+                name: "UserModule");
+
+            migrationBuilder.DropTable(
+                name: "UserRefreshToken");
+
+            migrationBuilder.DropTable(
+                name: "Group");
+
+            migrationBuilder.DropTable(
+                name: "Module");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "Institution");
+
+            migrationBuilder.DropTable(
+                name: "UserType");
+        }
+    }
+}
