39592055f8a972f68b4af8342c72e3769a31390d
Thabo <madunathabo2@gmail.com>
2022-09-23 15:17:29 +0200

    (API): create new entities

---
+++ APIs/TutorMe/TutorMe/Entities/IRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace TutorMe.Entities {
+    public class IRequest {
+        public Guid RequestId { get; set; }
+        public Guid TuteeId { get; set; }
+        public Guid TutorId { get; set; }
+        public string DateCreated { get; set; }
+        public Guid ModuleId { get; set; }
+    }
+}
