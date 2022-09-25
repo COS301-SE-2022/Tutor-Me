2f039efb36e10315b04071492f487db20f4c58ad
SimphiweNdlovu <u19027372@tuks.co.za>
2022-09-25 21:41:13 +0200

    configure the path of app settings on Integration tests

--- APIs/TutorMe/Tests/IntegrationTests/WebAppFactory.cs
+++ APIs/TutorMe/Tests/IntegrationTests/WebAppFactory.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TutorMe.Data;
 
@@ -11,8 +12,15 @@ public class WebAppFactory: WebApplicationFactory<Program>
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        builder.ConfigureTestServices(services =>
+        builder.ConfigureAppConfiguration(config =>
         {
+            var projectDir = Directory.GetCurrentDirectory();
+            var configPath = Path.Combine(projectDir, "appsettings.json");
+            config.AddJsonFile(configPath);
+        });
+        builder.ConfigureTestServices(services =>
+        { 
+        
             var descriptor = services.SingleOrDefault(
                 d => d.ServiceType == typeof(DbContextOptions<TutorMeContext>));
                             
