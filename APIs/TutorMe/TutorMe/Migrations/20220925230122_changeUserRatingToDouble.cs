2757748c0121592f2e4c4e3a4a506f2d92d8680d
Thabo <madunathabo2@gmail.com>
2022-09-28 15:38:27 +0200

    (API): add authentication in file system

---
+++ APIs/TutorMe/TutorMe/Migrations/20220925230122_changeUserRatingToDouble.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TutorMe.Migrations
+{
+    public partial class changeUserRatingToDouble : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "verified",
+                table: "User",
+                newName: "Verified");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "rating",
+                table: "User",
+                type: "float",
+                nullable: true,
+                defaultValue: 0.0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true,
+                oldDefaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Verified",
+                table: "User",
+                newName: "verified");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "rating",
+                table: "User",
+                type: "int",
+                nullable: true,
+                defaultValue: 0,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true,
+                oldDefaultValue: 0.0);
+        }
+    }
+}
