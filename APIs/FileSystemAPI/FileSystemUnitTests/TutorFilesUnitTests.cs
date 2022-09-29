d1c3d363a5ea458c956cbcdeaec94e94c6b123db
Thabo <madunathabo2@gmail.com>
2022-09-29 05:23:47 +0200

    File system unit tests

--- APIs/FileSystemAPI/FileSystemUnitTests/TutorFilesUnitTests.cs
+++ APIs/FileSystemAPI/FileSystemUnitTests/TutorFilesUnitTests.cs
@@ -138,62 +138,31 @@ public class UserFilesControllerUnitTests
 
 
 
-    //[Fact]
-    //public async Task DeleteUserFilesById_Returns_true()
-    //{
+    [Fact]
+    public async Task DeleteUserFilesById_ReturnsUauthorized() {
 
-    //    Arrange
-    //    var userAuth = new UserAuth
-    //    {
-    //        Email = "madunathabo2@gmail.com",
-    //        Password = "Tutorme#1",
-    //        TypeId = new Guid("98CA5264-1266-4158-82B6-5DE7FDD03599")
-    //    };
-    //    var UserFiles = new IUserFiles
-    //    {
-    //        Id = Guid.NewGuid(),
-    //        UserImage = Guid.NewGuid().ToByteArray(),
-    //        UserTranscript = Guid.NewGuid().ToByteArray()
-    //    };
+        //Arrange
+        var userAuth = new UserAuth {
+            Email = "madunathabo2@gmail.com",
+            Password = "Tutorme#1",
+            TypeId = new Guid("98CA5264-1266-4158-82B6-5DE7FDD03599")
+        };
+        var userFiles = new IUserFiles {
+            Id = Guid.NewGuid(),
+            UserImage = Guid.NewGuid().ToByteArray(),
+            UserTranscript = Guid.NewGuid().ToByteArray()
+        };
 
-    //    _UserFilesRepositoryMock.Setup(repo => repo.DeleteUserFilesById(It.IsAny<Guid>())).Returns(true);
-    //    var controller = new UserFilesController(_UserFilesRepositoryMock.Object, _mapper.Object);
+        _UserFilesRepositoryMock.Setup(repo => repo.DeleteUserFilesById(It.IsAny<Guid>())).Returns(true);
+        var controller = new UserFilesController(tutorMeContext, _UserFilesRepositoryMock.Object);
 
-    //    Act
-    //    var result = controller.DeleteUserFiles(expectedTutor.UserFilesId);
-    //    Assert
-    //    Assert.NotNull(result);
-    //    Assert.IsType<OkObjectResult>(result);
-    //    var actual = (result as OkObjectResult).Value;
-    //    Assert.IsType<Boolean>(actual);
-    //    Assert.Equal(true, actual);
+        //Act
+        var result = controller.DeleteUserFiles(userFiles.Id, userAuth.Email, userAuth.Password, userAuth.TypeId);
+        //Assert
+        Assert.NotNull(result);
+        Assert.IsType<UnauthorizedResult>(result.Result);
 
-    //}
-
-    //[Fact]
-    //public async Task DeleteUserFilesById_Returns_False()
-    //{
-
-    //    //Arrange
-    //    var expectedTutor = new UserFiles
-    //    {
-    //        Name = "University Of Pretoria",
-
-    //        Location = "Hatfield"
-    //    };
-
-    //    _UserFilesRepositoryMock.Setup(repo => repo.deleteUserFilesById(It.IsAny<Guid>())).Returns(false);
-    //    var controller = new UserFilesController(_UserFilesRepositoryMock.Object, _mapper.Object);
-
-    //    //Act
-    //    var result = controller.DeleteUserFiles(expectedTutor.UserFilesId);
-    //    // Assert
-    //    Assert.NotNull(result);
-    //    Assert.IsType<OkObjectResult>(result);
-    //    var actual = (result as OkObjectResult).Value;
-    //    Assert.IsType<Boolean>(actual);
-    //    Assert.Equal(false, actual);
-
-    //}
-
+        var actual = (result.Result as UnauthorizedResult);
+        Assert.IsType<UnauthorizedResult>(actual);
+    }
 }
